2ad6a7f316471d37af5da8146cfb2be18ab12025
Brad Yip <37090807+BradYip@users.noreply.github.com>
2019-03-29 15:53:51 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('resize', function(){
+    writeDimensions();
+});
+
+window.addEventListener('load', function() {
+    writeDimensions();
+});
+
+function writeDimensions(){
+    var myWidth = document.body.scrollWidth;
+    var myHeight = document.body.scrollHeight;
+    
+    var span = document.getElementById("myHeight");
+    span.textContent = myHeight;
+
+    var span2 = document.getElementById("myWidth");
+    span2.textContent = myWidth;
+}
